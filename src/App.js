a5c37e31e985d517d0c454691ff734ed8b90ec92
Jessica <duellje@oregonstate.edu>
2020-10-25 21:49:08 -0400

    getting data from api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Character from "./components/Character";
+import "./App.css";
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [pokemonList, setPokemon] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios
+      .get(" https://pokeapi.co/api/v2/pokedex/kanto")
+      .then((res) => {
+        console.log(res);
+        setPokemon(res.data.pokemon_entries);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character pokemonList={pokemonList} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
-// Write your Character component here
+import React from "react";
+
+const Character = (props) => {
+  const { pokemonList } = props;
+
+  return (
+    <div>
+      {pokemonList.map((pokemon) => {
+        return pokemon.pokemon_species.name;
+      })}
+    </div>
+  );
+};
+
+export default Character;
